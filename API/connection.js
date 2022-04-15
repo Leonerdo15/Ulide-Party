9177856b50c45f1a396b5b526d321d0af442e90f
Felipe Campelo Sabbado <fsabbado@gmail.com>
2022-04-15 12:53:22 +0100

    api

--- API/connection.js
+++ API/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-const connectionString = process.env.CONNECTION
+const connectionString = "postgres://d8h94rq9ds518t:0d99e34438ea8c15e2ebbd1af7dc272773a28c914271872dab574eeb169181c1@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/cfnltzahxgvckj"
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
