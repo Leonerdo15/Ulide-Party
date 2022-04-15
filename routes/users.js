62cbee51f05b10b6fb40a56a79ee5de1fc839513
Felipe Campelo Sabbado <fsabbado@gmail.com>
2022-04-15 12:40:31 +0100

    api testes

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,10 @@ const express = require('express');
 const client = require("../API/connection");
 const router = express.Router();
 
-client.connect();
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  client.query('Select * from users', (err, result)=>{
-    if(!err){
-      res.send(result.rows);
-    }
-  });
-  client.end;
+  let users=[{name:"John Doe", birthDate:"19/02/1999"},{name:"John Lock", birthDate: "06/08/1782"}];
+  res.send(users);
 });
 
 module.exports = router;
